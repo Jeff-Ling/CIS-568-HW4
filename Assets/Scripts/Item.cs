e0ad3da0251cc3235da8cb330e6fda4e1b4c9f69
Alex <28486541+IwakuraRein@users.noreply.github.com>
2024-02-18 18:19:16 -0500

    Update

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -14,18 +14,6 @@ namespace MyFirstARGame
         public virtual void OnDrop(Item item)
         {
 
-        }
-
-        // Start is called before the first frame update
-        public virtual void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        public virtual void Update()
-        {
-        
-        }
+        }s
     }
 }
--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -20,17 +20,5 @@ namespace MyFirstARGame
         {
 
         }
-
-        // Start is called before the first frame update
-        public virtual void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        public virtual void Update()
-        {
-        
-        }
     }
 }
